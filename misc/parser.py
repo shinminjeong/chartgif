4eb739b4aaca83db2b2801fd97eda71109362a6f
Minjeong Shin <minjeong.shin@anu.edu.au>
2020-04-12 12:17:22 +0900

    update color and timetable

--- misc/parser.py
+++ misc/parser.py
@@ -1,6 +1,14 @@
 import os, csv
 import drawSvg as draw
 
+color = [["#00D2CB","#009E98","#006966"],
+        ["#FFC702","#BF9502","#806401"],
+        ["#83C1FF","#6291BF","#52799F","#314860","#101820"]]
+legends = ["Hand gestures", "Video editing", "Video playback"]
+reasons = [["pointing","moving","shaping"],
+        ["labeling","tracing","flickering"],
+        ["pause","slow down","move fast","go backward","instant replay"]]
+
 def parseTimetable(filename):
     videos = []
     reader = csv.reader(open(filename))
@@ -22,6 +30,9 @@ def parseTimetable(filename):
 
         s, e = r[1].split(":"), r[2].split(":")
         t1, t2, t3 = r[3], r[5], r[7]
+        r1 = reasons[0].index(r[4]) if r[4].strip() else -1
+        r2 = reasons[1].index(r[6]) if r[6].strip() else -1
+        r3 = reasons[2].index(r[8]) if r[8].strip() else -1
         s_sec = int(s[0])*60+int(s[1])
         e_sec = int(e[0])*60+int(e[1])
         times.append(s_sec)
@@ -29,7 +40,8 @@ def parseTimetable(filename):
         video["timeline"].append({
             "st": s_sec,
             "et": e_sec,
-            "pattern": [t1, t2, t3]
+            "pattern": [t1, t2, t3],
+            "reason": [r1, r2, r3]
         })
     if video:
         video["starttime"] = min(times)
@@ -41,13 +53,10 @@ def parseTimetable(filename):
 videos = parseTimetable("timetable.csv")
 # print(videos)
 
-
-color = ["#00D2CB", "#FFC702", "#83A3FF"]
-legends = ["Hand gestures", "Video editing", "Video playback"]
 h_pattern = 20
 h_margin = h_pattern*3+40
 top_margin = 80
-left_margin = 640
+left_margin = 700
 right_margin = 20
 bottom_margin = 80
 t_width = 2000
@@ -56,9 +65,9 @@ d = draw.Drawing(left_margin+t_width+right_margin, top_margin+t_height)
 d.append(draw.Rectangle(0, 0, t_width+left_margin+right_margin, t_height, fill="white"))
 
 for i, l in enumerate(legends):
-    d.append(draw.Rectangle(left_margin+480+400*i, t_height-top_margin+40, 30, 30, fill=color[i]))
-    d.append(draw.Text(l, 24, left_margin+480+400*i+48, t_height-top_margin+48, center=0, fill="black"))
-
+    for j, c in enumerate(color[i]):
+        d.append(draw.Rectangle(left_margin+480+400*i+j*30, t_height-top_margin+40, 30, 30, fill=c))
+    d.append(draw.Text(l, 24, left_margin+480+400*i+18+len(color[i])*30, t_height-top_margin+48, center=0, fill="black"))
 
 for i, v in enumerate(videos):
     print(v)
@@ -69,10 +78,10 @@ for i, v in enumerate(videos):
     for frame in v["timeline"]:
         x = t_width*(frame["st"]-v["starttime"])/w100
         width = t_width*(frame["et"]-frame["st"])/w100
-        for p, pattern in enumerate(frame["pattern"]):
-            if (pattern == "Y"):
+        for p, pattern in enumerate(zip(frame["pattern"],frame["reason"])):
+            if (pattern[0] == "Y"):
                 # print(x, (p+1)*h_pattern+h_margin*i, h_margin*i)
-                d.append(draw.Rectangle(left_margin+x, t_height-top_margin-(p+1)*h_pattern-h_margin*i, width, h_pattern, fill=color[p]))
+                d.append(draw.Rectangle(left_margin+x, t_height-top_margin-(p+1)*h_pattern-h_margin*i, width, h_pattern, fill=color[p][pattern[1]]))
     # break
 
 # time axis
